81a9fc9ec51dae53b64934c093a0d7f32a335187
AlexTer59 <alex.ter.61660z2@gmail.com>
2024-08-15 16:15:39 +0500

    Created my_notes model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    note_status = models.CharField(max_length=128)
+    note_text = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Заметка'
+
