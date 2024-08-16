1810d017be4512d01391db3c3418643b42fd8595
AlexTer59 <alex.ter.61660z2@gmail.com>
2024-08-16 13:35:53 +0500

    Created urls, modified my_notes model

--- core/models.py
+++ core/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Note(models.Model):
-    note_status = models.CharField(max_length=128)
-    note_text = models.CharField(max_length=256)
+    status = models.BooleanField(default=False)
+    text = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
