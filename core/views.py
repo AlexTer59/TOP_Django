33f59c2e5118d53827a0de37047028390222778b
AlexTer59 <alex.ter.61660z2@gmail.com>
2024-08-16 16:01:53 +0500

    Created template, config styles, modified main view and migrate

--- core/views.py
+++ core/views.py
@@ -3,5 +3,7 @@ from .models import Note
 
 
 def main(request):
-    notes = Note.objects.all()
-    return render(request, 'index.html', {'notes': notes})
+    completed_notes = Note.objects.filter(status=True)
+    uncompleted_notes = Note.objects.filter(status=False)
+    return render(request, 'index.html',
+                  {'completed_notes': completed_notes, 'uncompleted_notes': uncompleted_notes})
