5f4d81335d7a279bc3b47243a9d14b355da4e446
AlexTer59 <alex.ter.61660z2@gmail.com>
2024-08-20 15:36:41 +0500

    Created add_task template and added the ability to create tasks from this template

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import main
+from .views import main, add_task, add_task_submit
 
 urlpatterns = [
-    path('', main)
+    path('', main, name='tasks'),
+    path('add_task', add_task, name='add_task'),
+    path('add_task_submit', add_task_submit, name='add_task_and_submit')
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Note
 
 
 def main(request):
     completed_notes = Note.objects.filter(status=True)
     uncompleted_notes = Note.objects.filter(status=False)
-    return render(request, 'index.html',
+    return render(request, 'task_list.html',
                   {'completed_notes': completed_notes, 'uncompleted_notes': uncompleted_notes})
+
+
+def add_task(request):
+    return render(request, 'add_task.html')
+
+
+def add_task_submit(request):
+    task = request.POST.get('task')
+    status = True if request.POST.get('is_completed') == 'on' else False
+    Note.objects.create(text=task, status=status)
+    return redirect('tasks')
